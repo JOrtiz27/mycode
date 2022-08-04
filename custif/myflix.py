826dfe4d240a865f8e9483e353278ce9b30bf57f
Joseph ortiz <JOrtiz27@users.noreply.github.com>
2022-08-04 15:27:26 +0000

    while looping

---
+++ custif/myflix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Battle of Belleau Wood Casualties 
+
+#tallies up scores by using count varibles 
+
+#Welcome messages
+print("Facts of the USMC Battle of Belleau Woods")
+#start
+#quiz with their answers 
+German Casualties = (input("how many german casualties were there?:")
+USMC   Causualties 
+#shows the conditionals that displays answers even if they get it wrong but will tally up the points if they get it right.
+    if(German Casualties == "9,500" or German Casualties == "11,100"):
+            count+=100
+    else(German Casualties): 
+    print(" 9,500 casualties, including 1,600 captured.") 
